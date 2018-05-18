fe69ce5eae5da9702fe79cda0eec427cac08fc94
Annie Bourke <aabourke@gmail.com>
2018-05-18 23:03:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,21 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :new
+    end
+
+    post '/show' do
+      @student = Student.new(params[:student])
+
+      params[:student][:courses].each do |details|
+        Course.new(details)
+      end
+
+      @courses = Course.all
+
+      erb :show
+    end
 
   end
 end
